2f3694f5654d6f492bc51cc5f1f9f81989dc0352
MrLYC <imyikong@gmail.com>
2023-05-24 00:35:04 +0800

    build: update

--- integration-tests/start.sh
+++ integration-tests/start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 root_dir="$(dirname $0)"
+export CMDR_LOG_LEVEL=debug CMDR_CORE_CONFIG_PATH=/tmp/cmdr.yaml CMDR_CORE_ROOT_DIR=$(pwd)/.cmdr CMDR_CORE_PROFILE_DIR=$(pwd)/profile
 
 set -e
-export CMDR_LOG_LEVEL=debug CMDR_CORE_CONFIG_PATH=/tmp/cmdr.yaml CMDR_CORE_ROOT_DIR=$(pwd)/.cmdr CMDR_CORE_PROFILE_DIR=$(pwd)/profile
 
 ./install.sh -d 30
 
