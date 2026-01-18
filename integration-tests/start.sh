9109d67e7a27c05648c497066dce0562f49222f5
MrLYC <imyikong@gmail.com>
2026-01-18 23:43:49 +0800

    ci: build local binary for integration tests

--- integration-tests/start.sh
+++ integration-tests/start.sh
@@ -5,7 +5,9 @@ root_dir="$(dirname $0)"
 set -e
 export CMDR_LOG_LEVEL=debug CMDR_CORE_CONFIG_PATH=/tmp/cmdr.yaml CMDR_CORE_ROOT_DIR=$(pwd)/.cmdr CMDR_CORE_PROFILE_DIR=$(pwd)/profile
 
-./install.sh -d 30
+if [ "${SKIP_INSTALL}" != "true" ]; then
+  ./install.sh -d 30
+fi
 
 source ./profile/cmdr_initializer.sh
 
